ff56ece0ce791e569c5f366c4d4ac92d4542696d
JulianMella <juliaame@uio.no>
2025-05-21 23:03:42 +0200

    one step closer

---
+++ front/script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('api-form-js');
+
+
+    form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const apiKey = form.apiKey.value;
+
+    });
+});
